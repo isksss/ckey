a77642b2b3f4bd8973719e8ec5b4f486c7b1b94c
isksss <104404522+isksss@users.noreply.github.com>
2023-11-03 19:41:57 +0900

    add: option

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
+use clap::{Parser, ArgGroup};
+
+#[derive(Debug, Parser)]
+#[command(group(ArgGroup::new("").required(true).args(["init", "set", "list"])))]
+struct Args {
+    #[clap(short = 'i', long = "init", help = "")]
+    init: bool,
+
+    #[clap(short = 's', long = "set", help = "")]
+    set: bool,
+
+    #[clap(short = 'l', long = "list", help = "")]
+    list: bool,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Args::parse();
+    let init = args.init;
+    if init {
+        println!("init");
+    } else {
+        println!("no init");
+    }
 }
